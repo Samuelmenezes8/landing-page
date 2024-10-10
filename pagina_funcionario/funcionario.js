1acac69912be8e0d9069da842c7e71f6761476f9
Samuel Menezes <samuelmen.soares@gmail.com>
2024-10-09 23:48:54 -0300

    landing-page
    atualizando

--- pagina_funcionario/funcionario.js
+++ pagina_funcionario/funcionario.js
@@ -1,19 +1,25 @@
 function toggleProductDetails(id) {
     const productDetail = document.getElementById(`productDetail${id}`);
+    const button = document.querySelector(`[aria-controls="productDetail${id}"]`);
 
-    // Verifica se a div está visível e aplica a classe de expansão ou contração
     if (productDetail.style.display === "none" || !productDetail.style.display) {
         productDetail.classList.add("expand");
         productDetail.classList.remove("collapse");
+        button.setAttribute("aria-expanded", "true");
     } else {
         productDetail.classList.add("collapse");
         productDetail.classList.remove("expand");
+        button.setAttribute("aria-expanded", "false");
     }
 
-    // Alterna a visibilidade
-    if (productDetail.style.display === "none" || !productDetail.style.display) {
-        productDetail.style.display = "block";
-    } else {
-        productDetail.style.display = "none";
-    }
+    productDetail.style.display = productDetail.style.display === "none" ? "block" : "none";
 }
+
+    // Verifica se a div está visível e aplica a classe de expansão ou contração
+    // Alterna a visibilidade
+    // if (productDetail.style.display === "none" || !productDetail.style.display) {
+    //     productDetail.style.display = "block";
+    // } else {
+    //     productDetail.style.display = "none";
+    // }
+
